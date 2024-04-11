1d9a4b81a27a4c7997bf7c7683a3a49b23558de0
serii <seriiburduja@gmail.com>
2024-04-11 16:31:38 +0300

    feat: 08

---
+++ 08.convertNumberToString.py
@@ -0,0 +1,6 @@
+# We need a function that can transform a number into a string.
+def convertNumberToString(number):
+    return str(number)
+
+print(convertNumberToString(42)) # "42"
+print(convertNumberToString(-42)) # "-42"
