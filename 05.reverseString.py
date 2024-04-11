b06680607c98b354a1f2f9b4f8b8008645e479e5
serii <seriiburduja@gmail.com>
2024-04-11 16:26:13 +0300

    feat: 05

---
+++ 05.reverseString.py
@@ -0,0 +1,6 @@
+# Complete the solution so that it reverses the string passed into it.
+def reverseString(string):
+    print(string[::-1])
+
+reverseString("Hello") # olleH
+reverseString("World") # dlroW
