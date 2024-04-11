c57f6383b53f167d39c8e9c018cb99cad64c91c9
serii <seriiburduja@gmail.com>
2024-04-11 16:55:29 +0300

    feat: 14

---
+++ 14.descendingOrder.py
@@ -0,0 +1,5 @@
+# ‘Your task s to make a function that can take any non-negative integer as an
+# rearrange the cits to create the highest possible number.
+# x gument and return it with ts digits descending order. Essentially,
+def descendingOrder():
+
