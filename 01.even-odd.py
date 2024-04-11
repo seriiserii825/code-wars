69aa65fd364703ea7ef1e855d2a05409fe783a91
serii <seriiburduja@gmail.com>
2024-04-11 16:09:55 +0300

    feat: 01

---
+++ 01.even-odd.py
@@ -0,0 +1,8 @@
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def evenOrOdd(num):
+    result = "Even" if num % 2 == 0 else "Odd"
+    print(result)
+
+evenOrOdd(3) 
+
