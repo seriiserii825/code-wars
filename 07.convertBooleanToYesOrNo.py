55d77960b407df8751a605ab924413cac5e127ac
serii <seriiburduja@gmail.com>
2024-04-11 16:29:43 +0300

    feat: 07

---
+++ 07.convertBooleanToYesOrNo.py
@@ -0,0 +1,6 @@
+# Complete the method that takes a boolean value and returna "Yes" stringfor true ,ora “No” stringfor false.
+def convertBooleanToYesOrNo(bool):
+    return "Yes" if bool else "No"
+
+print(convertBooleanToYesOrNo(True)) # Yes
+print(convertBooleanToYesOrNo(False)) # No
