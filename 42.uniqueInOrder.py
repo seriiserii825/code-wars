a1fde566c46946ffb61648ede04d6cde34302700
serii <seriiburduja@gmail.com>
2024-04-17 17:52:57 +0300

    feat: 42

--- 42.uniqueInOrder.py
+++ 42.uniqueInOrder.py
@@ -3,6 +3,7 @@ def uniqueInOrder(iterable):
     for i in range(len(iterable)):
         if i == 0 or iterable[i] != iterable[i - 1]:
             s.append(iterable[i])
+            print(f's: {s}')
     return s
 
 print(uniqueInOrder('aaabbbaaacccdd'))
